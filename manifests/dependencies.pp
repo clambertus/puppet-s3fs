86ea95726c1a50679c1dfc8d6519e258781e4dca
cml <cml@table2.com>
2014-11-23 02:07:09 +0000

    updated to support Ubuntu 14.04 which does not have the fuse-utils package

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -2,7 +2,13 @@ class s3fs::dependencies {
 
   if ! defined(Package['build-essential'])      { package { 'build-essential':      ensure => installed } }
   if ! defined(Package['libfuse-dev'])          { package { 'libfuse-dev':          ensure => installed } }
-  if ! defined(Package['fuse-utils'])           { package { 'fuse-utils':           ensure => installed } }
+
+  if $operatingsystem == 'Ubuntu' {
+    if $lsbmajdistrelease < '14' {
+      if ! defined(Package['fuse-utils'])           { package { 'fuse-utils':           ensure => installed } }
+    }
+  }
+
   if ! defined(Package['libcurl4-openssl-dev']) { package { 'libcurl4-openssl-dev': ensure => installed } }
   if ! defined(Package['libxml2-dev'])          { package { 'libxml2-dev':          ensure => installed } }
   if ! defined(Package['mime-support'])         { package { 'mime-support':         ensure => installed } }
