a20fd9f0fdb1949cf9adf6e2dcb04782e4da4fbc
Andrew Bayer <andrew.bayer@gmail.com>
2015-02-16 14:13:13 -0800

    Use GitHub for tarball, use newer version, build newer version properly

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class s3fs::params {
 
   $credentials_file = '/etc/passwd-s3fs'
   $download_dir       = '/tmp'
-  $download_url     = 'http://s3fs.googlecode.com/files'
+  $download_url     = 'https://github.com/s3fs-fuse/s3fs-fuse/archive'
 
   # Fail if the OS is not Ubuntu/Debian
   case $::operatingsystem {
@@ -19,12 +19,13 @@ class s3fs::params {
   }
 
   # s3fs version >1.19 requires fuse > 2.8.4 which is installed on >10.10
+  # s3fs version 1.19 doesn't work pretty much anywhere, so use newer 1.78.
   case $::lsbdistdescription {
     'Ubuntu 10.10': {
       $version = '1.61'
     }
     default: {
-      $version = '1.19'
+      $version = '1.78'
     }
   }
 
